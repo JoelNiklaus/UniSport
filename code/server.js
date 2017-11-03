40a4d0b8e7655ec9a57a77bec57353bf5f81bb52
Joel Niklaus <me@joelniklaus.ch>
2017-11-03 14:33:17 +0100

    some changes

--- code/server.js
+++ code/server.js
@@ -1,109 +1,91 @@
-
 const express = require('express');
-const bodyParser= require('body-parser')
-const app = express()
-const url ="mongodb://localhost:27017/names";
+const bodyParser = require('body-parser');
+const app = express();
+const url = "mongodb://localhost:27017/UniSport";
 
 
 const MongoClient = require('mongodb').MongoClient;
 
-var db
+var db;
 
 MongoClient.connect(url, (err, database) => {
-  if (err) return console.log(err)
-  db = database
-  
+    if (err) return console.log(err);
+    db = database
+
 });
-
-
-
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 
-app.use(express.static(__dirname+'/views'))
+app.use(express.static(__dirname + '/views'));
 
-app.get('/',function(req,res){
-  res.sendFile('index.html');
+app.get('/', function (req, res) {
+    res.sendFile('index.html');
 })
 
 
-
-  app.post('/insert', (req, res) => {
-  	  console.log(req.body);
-      var l = req.body.name;
-      var query = { 'name': l}
-            console.log(query);
-
-      
-
-  db.collection('insert').save(req.body, (err, result) => {
-   if (err) return console.log(err);
-
-   db.collection("insert").find(query).toArray(function(err, result) {
-    if (err) throw err;
-  res.set('Access-Control-Allow-Origin', '*');
-    console.log(result)
+app.post('/insert', (req, res) => {
+    console.log(req.body);
+    var l = req.body.name;
+    var query = {'name': l};
+    console.log(query);
 
 
-  res.json(result);
-    
+    db.collection('tasks').save(req.body, (err, result) => {
+        if (err) return console.log(err);
 
- });
-  });
+        db.collection("tasks").find(query).toArray(function (err, result) {
+            if (err) throw err;
+            res.set('Access-Control-Allow-Origin', '*');
+            console.log(result);
 
-  
+
+            res.json(result);
+
+
+        });
+    });
 
 
 });
 
 
 app.post('/delete', (req, res) => {
-        console.log("todelete");
+    console.log("todelete");
 
-      console.log(req.body);
-      var l = req.body.name;
-      var query = { 'name': l}
-            console.log(query);
+    console.log(req.body);
+    var l = req.body.name;
+    var query = {'name': l};
+    console.log(query);
 
-      
 
-  db.collection("insert").deleteMany(query, function(err, obj) {
-    if (err) throw err;
+    db.collection("tasks").deleteMany(query, function (err, obj) {
+        if (err) throw err;
 
-    console.log(obj.result.n+"   document deleted");
-    res.json(obj.result);
-  });
+        console.log(obj.result.n + "   document deleted");
+        res.json(obj.result);
+    });
 
-    
 
- });
+});
 
 app.get('/show', (req, res) => {
-        console.log("show");
-
-      
-
-  db.collection("insert").find({}).toArray(function(err, result) {
-    if (err) throw err;
-
-    console.log(result);
-    res.json(result);
-  });
-
-    
-
- });
-  
-  
+    console.log("show");
 
 
+    db.collection("tasks").find({}).toArray(function (err, result) {
+        if (err) throw err;
+
+        console.log(result);
+        res.json(result);
+    });
 
 
+});
 
 
-  app.listen(3000, () => {
+app.listen(3000, () => {
     console.log('listening on 3000')
-  });
+});
--- code/views/app.js
+++ code/views/app.js
@@ -1,99 +1,69 @@
 angular.module("SAMPLETUTO", [])
-.controller("SAMPLECTRL",  function($scope,$http) {
-	
-	
-
-	
-
-	$scope.insertIS = false;
-	$scope.deleteIS = false;
-	$scope.showIS = false;
+    .controller("SAMPLECTRL", function ($scope, $http) {
 
 
- 
+        $scope.insertIS = false;
+        $scope.deleteIS = false;
+        $scope.showIS = false;
 
 
-	 
-	
- 
-	$scope.add = function(formData) {
-		var data = { 'course_name': formData };
-		    	  $scope.deleteIS = false;
-    				$scope.showIS = false;
+        $scope.add = function (formData) {
+            var data = {'course_name': formData};
+            $scope.deleteIS = false;
+            $scope.showIS = false;
 
-        $scope.insertIS = true;
-        console.log(formData);
+            $scope.insertIS = true;
+            console.log(formData);
 
-        $http.post('/insert',formData).
-        then(function(response) {
-        	console.log(response.data[0].name);
-            $scope.insertdata=response.data[0].name;
-            console.log("posted successfully");
-                        $scope.formData="";
-        }).catch(function(response) {
-            console.error("error in posting");
-        });
-    }
+            $http.post('/insert', formData).then(function (response) {
+                console.log(response.data[0].name);
+                $scope.insertdata = response.data[0].name;
+                console.log("posted successfully");
+                $scope.formData = "";
+            }).catch(function (response) {
+                console.error("error in posting");
+            });
+        }
 
-    $scope.delete = function(formData) {
-    	        console.log(formData);
+        $scope.delete = function (formData) {
+            console.log(formData);
 
-    	$scope.insertIS = false;
-    				$scope.showIS = false;
+            $scope.insertIS = false;
+            $scope.showIS = false;
 
-    	  $scope.deleteIS = true;
+            $scope.deleteIS = true;
 
-		var data = { 'name': formData };
+            var data = {'name': formData};
 
-        $http.post('/delete',formData).
-        then(function(response) {
-        	
-            $scope.deletedata= response.data.n+"  task deleted ";
-            console.log(response);
-                        $scope.formData="";
+            $http.post('/delete', formData).then(function (response) {
 
-        }).catch(function(response) {
-            console.error("error in posting");
-                        $scope.deletedata="errooor";
+                $scope.deletedata = response.data.n + "  task deleted ";
+                console.log(response);
+                $scope.formData = "";
 
-        });
-    }
+            }).catch(function (response) {
+                console.error("error in posting");
+                $scope.deletedata = "errooor";
 
-	$scope.show = function() {
-			$scope.showIS = true;
-			$scope.insertIS = false;
-	$scope.deleteIS = false;
+            });
+        }
+
+        $scope.show = function () {
+            $scope.showIS = true;
+            $scope.insertIS = false;
+            $scope.deleteIS = false;
 
 
-        $http.get('/show').
-        then(function(response) {
-        	console.log(response.data);
-        	$scope.tasks=response.data;
-            console.log("get successfully");
-                        $scope.formData="";
+            $http.get('/show').then(function (response) {
+                console.log(response.data);
+                $scope.tasks = response.data;
+                console.log("get successfully");
+                $scope.formData = "";
 
-        }).catch(function(response) {
-            console.error("error in posting");
-        });
-    }
-
+            }).catch(function (response) {
+                console.error("error in posting");
+            });
+        }
 
 
-   
-
-
-
-	
-
-	 
-		
-    
-
-               
-
-
-	
-
-
-	
-});
+    });
